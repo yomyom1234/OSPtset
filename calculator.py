83b9b33700af6277d3eb29f23419a42cbc6d588e
NAME <test@gmail.com>
2023-11-23 10:21:14 +0900

    main

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 #Calculator
-def add(a, b);
-    return (a + b);
+def add(a, b):
+    return (a + b)
 
-def sub(a, b);
-    return (a - b);
+def sub(a, b):
+    return (a - b)
+
+def multi(a, b):
+    return (a * b)
+
+def devide_main(a, b):
+    return (a / b)
